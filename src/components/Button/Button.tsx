d6b603013995724d70bf42e8ac6d90ee894c15bc
Bruno Tenório <tenoriobn@gmail.com>
2024-12-16 16:57:00 -0300

    Finalizando desafio de documentar um componente que recebe um texto e uma props da tag

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,17 @@
-export default function Button({ children }) {
+import React from "react";
+
+interface ButtonProps {
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactNode;
+}
+
+export default function Button({ children, ...props }: ButtonProps) {
   return (
-    <button>{children}</button>
+    <button {...props}>{children}</button>
   )
+}
+
+Button.defaultProps = {
+  disabled: false,
 }
