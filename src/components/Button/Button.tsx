b7922262dc4b7d8db514b08ab91034024ea214b3
Bruno Tenório <tenoriobn@gmail.com>
2024-12-17 13:05:25 -0300

    Criando o tema do projeto

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button<ButtonProps>`
+  cursor: pointer;
+  border: 0;
+  padding: 8px 14px;
+  transition: 0.3s;
+  
+  ${({ theme, variant }) => `
+    background-color: ${theme.colors[variant].main};
+    color: ${theme.colors[variant].text};
+
+    &:hover {
+      background-color: ${theme.colors[variant].light};
+    }
+
+    &:focus {
+      background-color: ${theme.colors[variant].dark};
+    }
+  `}
+`;
 
 interface ButtonProps {
+  variant?: 'primary' | 'accent';
   disabled?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode;
 }
-
 export default function Button({ children, ...props }: ButtonProps) {
   return (
-    <button {...props}>{children}</button>
-  )
+    <StyledButton
+      {...props}
+    >
+      {children}
+    </StyledButton>
+  );
 }
 
 Button.defaultProps = {
   disabled: false,
+  variant: 'primary'
 }
