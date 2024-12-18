282a22ee131951c1691206028a3825a0c1468693
Bruno Tenório <tenoriobn@gmail.com>
2024-12-18 12:02:02 -0300

    customizando o tema da nossa documentação

---
+++ .storybook/DocTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Nome da Empresa',
+  brandUrl: 'www.alura.com.br',
+  brendImage: 'https://place-hold.it/350x150',
+  brandTarget: '_self'
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import DocTheme from './DocTheme';
+
+addons.setConfig({
+  theme: DocTheme
+})
